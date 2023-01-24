0ea34425bc3d825c107639f52ad0df8360c25827
UNordicc <73536467+UNordicc@users.noreply.github.com>
2023-01-24 20:09:03 +0100

    Update Socials.tsx

--- src/pages/main_page/Socials.tsx
+++ src/pages/main_page/Socials.tsx
@@ -5,11 +5,11 @@ import SocialsEntry from './SocialsEntry';
 const Socials: FunctionComponent = () => {
     return (
         <div className="greeting-socials">
-            <SocialsEntry index={0} image="/assets/revolt_r.svg" link="https://rvlt.gg/jan" text="@jan" />
-            <SocialsEntry index={1} image="/assets/discord.svg" link="https://discord.gg/4pZgvqgYJ8" text={"J\u0430n#2756"} />
-            <SocialsEntry index={2} image="/assets/github.svg" link="https://github.com/janderedev" text="JandereDev" />
-            <SocialsEntry index={3} image="/assets/misskey.svg" link="https://miruku.cafe/@jan" text="@jan@miruku.cafe" />
-            <SocialsEntry index={4} image="/assets/email.svg" link="==gCtFWasR3b60WZApWYuRWZyVGZlZnL4lne" text="K0WZApWYuRWZyVGZlZnL4lne" redactUntilClick={true} />
+            <SocialsEntry index={0} image="/assets/discord.svg" link="https://discord.com/users/265102629082890240" text={"UNordic#0782"} />
+            <SocialsEntry index={1} image="/assets/github.svg" link="https://github.com/UNordicc" text="UNordicc" />
+            <SocialsEntry index={2} image="/assets/twitter.svg" link="https://twitter.com/UNordicc" text="@UNordicc" />
+            <SocialsEntry index={3} image="/assets/email.svg" link="mailto:unordic@unordic.me" text="unordic@unordic.me" />
+            <SocialsEntry index={4} image="/assets/mastodon.svg" link="https://pol.social/@unordic" text="pol.social/@unordic" />
         </div>
     );
 }
