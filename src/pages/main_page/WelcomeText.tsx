fa67406e3fd3a2f54796b330924e8ea5ddc7d4eb
UNordicc <73536467+UNordicc@users.noreply.github.com>
2023-01-24 20:24:27 +0100

    Update WelcomeText.tsx

--- src/pages/main_page/WelcomeText.tsx
+++ src/pages/main_page/WelcomeText.tsx
@@ -6,7 +6,36 @@ const WelcomeText: FunctionComponent = () => {
         <div className="greeting-welcome">
             <span className="main-text">Hi.</span>
             <br/>
-            <span className="sub-text">I'm Jan.</span>
+            <span className="sub-text">I'm UNordic. </span>
+            <br/>
+            <img
+                src="https://github.com/twitter/twemoji/blob/master/assets/72x72/1f1f5-1f1f1.png?raw=true"
+                style={{
+                    position: "relative",
+                    left: "80%",
+                    transform: "translateX(-214%)",
+                    top: "20%",
+                }}
+            />
+            <img
+                src="/assets/fedora.png"
+                style={{
+                    position: "relative",
+                    left: "50%",
+                    transform: "translateX(-100%)",
+                    top: "20%",
+                }}
+            />
+             <img
+                src="/assets/arch.png"
+                style={{
+                    position: "relative",
+                    left: "50%",
+                    transform: "translateX(-65%)",
+                    top: "20%",
+                }}
+            />
+            <br/>
         </div>
     );
 }
