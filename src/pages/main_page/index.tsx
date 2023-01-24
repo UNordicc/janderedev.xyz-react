9346a5b0471ee99788a4750f109f65e4f4075ee5
UNordicc <73536467+UNordicc@users.noreply.github.com>
2023-01-24 20:25:00 +0100

    Update index.tsx

--- src/pages/main_page/index.tsx
+++ src/pages/main_page/index.tsx
@@ -7,7 +7,7 @@ const index: FunctionComponent = () => {
         <Greeting />
         <div style={{ height: "100vh" }}>
             <img
-                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstyles.redditmedia.com%2Ft5_3gvs7p%2Fstyles%2FcommunityIcon_q2rzyba8wg161.png%3Fwidth%3D256%26s%3D1790af2f987e48c21d8f940258e02d6c1005661f&f=1&nofb=1"
+                src="https://lanyard.cnrad.dev/api/265102629082890240"
                 style={{
                     position: "relative",
                     left: "50%",
