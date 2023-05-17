896838d837f596f42374ee0309dd350f089b79e8
UNordicc <73536467+UNordicc@users.noreply.github.com>
2023-05-17 20:19:29 +0200

    Update Socials.tsx

--- src/pages/main_page/Socials.tsx
+++ src/pages/main_page/Socials.tsx
@@ -8,9 +8,8 @@ const Socials: FunctionComponent = () => {
             <SocialsEntry index={0} image="/assets/discord.svg" link="https://discord.com/users/265102629082890240" text={"UNordic#0782"} />
             <SocialsEntry index={1} image="/assets/github.svg" link="https://github.com/UNordicc" text="UNordicc" />
             <SocialsEntry index={2} image="/assets/twitter.svg" link="https://twitter.com/UNordicc" text="@UNordicc" />
-            <SocialsEntry index={3} image="/assets/twitter.svg" link="https://twitter.com/assfolder" text="@assfolder" />
-            <SocialsEntry index={4} image="/assets/email.svg" link="mailto:unordic@unordic.me" text="unordic@unordic.me" />
-            <SocialsEntry index={5} image="/assets/mastodon.svg" link="https://pol.social/@unordic" text="pol.social/@unordic" />
+            <SocialsEntry index={3} image="/assets/email.svg" link="mailto:unordic@unordic.me" text="unordic@unordic.me" />
+            <SocialsEntry index={4} image="/assets/mastodon.svg" link="https://pol.social/@unordic" text="pol.social/@unordic" />
         </div>
     );
 }
