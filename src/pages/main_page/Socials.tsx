33e1495e615ddb32d8b8b51028301b04df7a5818
UNordicc <73536467+UNordicc@users.noreply.github.com>
2023-06-18 11:26:44 +0200

    Update Socials.tsx

--- src/pages/main_page/Socials.tsx
+++ src/pages/main_page/Socials.tsx
@@ -5,11 +5,9 @@ import SocialsEntry from './SocialsEntry';
 const Socials: FunctionComponent = () => {
     return (
         <div className="greeting-socials">
-            <SocialsEntry index={0} image="/assets/discord.svg" link="https://discord.com/users/265102629082890240" text={"UNordic#0782"} />
+            <SocialsEntry index={0} image="/assets/discord.svg" link="https://discord.com/users/265102629082890240" text={"UNordic"} />
             <SocialsEntry index={1} image="/assets/github.svg" link="https://github.com/UNordicc" text="UNordicc" />
             <SocialsEntry index={2} image="/assets/twitter.svg" link="https://twitter.com/UNordicc" text="@UNordicc" />
-            <SocialsEntry index={3} image="/assets/email.svg" link="mailto:unordic@unordic.me" text="unordic@unordic.me" />
-            <SocialsEntry index={4} image="/assets/mastodon.svg" link="https://pol.social/@unordic" text="pol.social/@unordic" />
         </div>
     );
 }
