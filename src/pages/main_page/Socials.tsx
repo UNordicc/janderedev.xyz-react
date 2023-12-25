2efae1d018e0e903256215618cc7f8a863f0730f
UNordicc <73536467+UNordicc@users.noreply.github.com>
2023-12-25 13:49:20 +0100

    Update Socials.tsx

--- src/pages/main_page/Socials.tsx
+++ src/pages/main_page/Socials.tsx
@@ -8,7 +8,8 @@ const Socials: FunctionComponent = () => {
             <SocialsEntry index={0} image="/assets/discord.svg" link="https://discord.com/users/265102629082890240" text={"UNordic"} />
             <SocialsEntry index={1} image="/assets/github.svg" link="https://github.com/UNordicc" text="UNordicc" />
             <SocialsEntry index={2} image="/assets/twitter.svg" link="https://twitter.com/UNordicc" text="@UNordicc" />
-            <SocialsEntry index={3} image="/assets/spacehey_icon_white.png" link="https://spacehey.com/unordic" text="UNordic" />
+            <SocialsEntry index={3} image="/assets/logo.svg" link="https://bsky.app/profile/unordic.bsky.social" text="@unordic.bsky.social" />
+            <SocialsEntry index={4} image="/assets/spacehey_icon_white.png" link="https://spacehey.com/unordic" text="UNordic" />
         </div>
     );
 }
