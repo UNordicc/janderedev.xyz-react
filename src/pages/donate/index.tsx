bee9004e171be773c4ffafc5225157bbfdd27e8c
UNordicc <73536467+UNordicc@users.noreply.github.com>
2024-09-16 21:20:46 +0200

    Update index.tsx

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -3,7 +3,6 @@ import { CryptoField } from './CryptoField';
 import './donate.css';
 
 const ADDRESSES = {
-    xmr: "4BBEM3MHtFfPAVwgQ5CRJiFdp2zVbF6aL3RKX1w6RnfjbW3L3RFaknc9bbnGrAFAZG2N6XpsGwpMn9yajG1mDfkr6X7MUgD",
     btc: "33dc7D5xgoF9fSXoWwwf7phoSkNMw6WExR",
     eth: "0xd68e47e7593902fDfAa77E650fEcbB133369058F",
     ltc: "ltc1qq5v0mwt8yu789n20ltf6sz5s83se0ycfw6c4we",
@@ -19,7 +18,6 @@ const index: FunctionComponent = () => {
                 If I don't support your favorite crypto or payment method, use <a href="https://bisq.network/">Bisq</a>, <a href="https://dex.robosats.com/">Robosats</a> or <a href="https://haveno.exchange/">Haveno</a> and buy there cryptocurrency without KYC.
                 For PLN use <a href="https://coinget.pl/">coinGet</a> or <a href="https://coincher.com">coincher</a>
             </p>
-            <CryptoField name="Monero" image="/assets/monero.png" address={ADDRESSES.xmr} />
             <CryptoField name="Bitcoin" image="/assets/bitcoin.png" address={ADDRESSES.btc} />
             <CryptoField name="Litecoin" image="/assets/litecoin.png" address={ADDRESSES.ltc} />
             <CryptoField name="ETH/USDT/USDC" image="/assets/ethereum.png" address={ADDRESSES.eth} />
