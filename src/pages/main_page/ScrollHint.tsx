5fc39e9f8971eea455e947f400542176cecb4ac5
UNordicc <73536467+UNordicc@users.noreply.github.com>
2024-09-16 21:04:26 +0200

    Update ScrollHint.tsx

--- src/pages/main_page/ScrollHint.tsx
+++ src/pages/main_page/ScrollHint.tsx
@@ -1,26 +1,26 @@
-import { FunctionComponent, useEffect, useState } from "react";
-import './greeting.css';
+#import { FunctionComponent, useEffect, useState } from "react";
+#import './greeting.css';
 
-const ScrollHint: FunctionComponent = () => {
-    const [arrowHidden, setArrowHidden] = useState(true);
-    const [textHidden, setTextHidden] = useState(true);
+#const ScrollHint: FunctionComponent = () => {
+    #const [arrowHidden, setArrowHidden] = useState(true);
+    #const [textHidden, setTextHidden] = useState(true);
 
-    useEffect(() => {
-        setTimeout(() => setArrowHidden(false), 2000);
-        setTimeout(() => setTextHidden(false), 3000);
-    });
+    #useEffect(() => {
+        #setTimeout(() => setArrowHidden(false), 2000);
+        #setTimeout(() => setTextHidden(false), 3000);
+    #});
 
-    return (
-        <div className="scroll-hint-container">
-            <span className={`${textHidden ? 'hidden' : ''}`}>Scroll down</span>
-            <br/>
-            <img
-                src="/assets/pointer_down.svg"
-                className={`${arrowHidden ? 'hidden' : ''}`}
-                onClick={() => window.scroll({ behavior: 'smooth', top: window.innerHeight })}
-            />
-        </div>
-    );
-}
+    #return (
+        #<div className="scroll-hint-container">
+            #<span className={`${textHidden ? 'hidden' : ''}`}>Scroll down</span>
+            #<br/>
+            #<img
+                #src="/assets/pointer_down.svg"
+                #className={`${arrowHidden ? 'hidden' : ''}`}
+                #onClick={() => window.scroll({ behavior: 'smooth', top: window.innerHeight })}
+            #/>
+        #</div>
+    #);
+#}
 
-export default ScrollHint;
+#export default ScrollHint;
