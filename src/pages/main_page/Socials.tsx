c701ae8eb6617f6bb289f76ded7af5722b29251e
UNordicc <73536467+UNordicc@users.noreply.github.com>
2026-01-11 01:56:15 +0100

    Fix duplicate SocialsEntry for bezpieczny.link

--- src/pages/main_page/Socials.tsx
+++ src/pages/main_page/Socials.tsx
@@ -8,7 +8,7 @@ const Socials: FunctionComponent = () => {
             <SocialsEntry index={0} image="/assets/discord.svg" link="https://discord.com/users/265102629082890240" text={"UNordic"} />
             <SocialsEntry index={1} image="/assets/github.svg" link="https://github.com/UNordicc" text="UNordicc" />
             <SocialsEntry index={2} image="/assets/x.svg" link="https://twitter.com/UNordicc" text="@UNordicc" />
-            <SocialsEntry index={3} image="/assets/download.svg" link="https://bsky.app/profile/bezpieczny.linl" text="@bezpieczny.link" />
+            <SocialsEntry index={3} image="/assets/download.svg" link="https://bsky.app/profile/bezpieczny.link" text="@bezpieczny.link" />
             <SocialsEntry index={4} image="/assets/spacehey_icon_white.png" link="https://spacehey.com/unordic" text="UNordic" />
             <SocialsEntry index={5} image="/assets/mastodon.svg" link="https://101010.pl/@unordic" text="@unordic@101010.pl" />
         </div>
